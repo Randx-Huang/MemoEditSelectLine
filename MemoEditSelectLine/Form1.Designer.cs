66751fd0555ef0ce496bb34734a8c3a712d93fe5
RandxHuang <wind04181986@gmail.com>
2018-12-28 15:57:39 +0800

    Add MemoEditSelectLine Solution

---
+++ MemoEditSelectLine/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MemoEditSelectLine
+{
+    partial class frmMemoEditTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.memoEdit1 = new DevExpress.XtraEditors.MemoEdit();
+            this.txtSearch = new DevExpress.XtraEditors.TextEdit();
+            this.btnSearch = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSearch.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // memoEdit1
+            // 
+            this.memoEdit1.EditValue = "";
+            this.memoEdit1.Location = new System.Drawing.Point(46, 49);
+            this.memoEdit1.Name = "memoEdit1";
+            this.memoEdit1.Properties.HideSelection = false;
+            this.memoEdit1.Size = new System.Drawing.Size(227, 189);
+            this.memoEdit1.TabIndex = 2;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(46, 21);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(146, 20);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(198, 20);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "搜尋";
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // frmMemoEditTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 260);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.memoEdit1);
+            this.Name = "frmMemoEditTest";
+            this.Text = "MemoEditTest";
+            this.Load += new System.EventHandler(this.frmMemoEditTest_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSearch.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.MemoEdit memoEdit1;
+        private DevExpress.XtraEditors.TextEdit txtSearch;
+        private DevExpress.XtraEditors.SimpleButton btnSearch;
+    }
+}
+
---
+++ MemoEditSelectLine/Form1.cs
@@ -0,0 +1,58 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MemoEditSelectLine
+{
+    public partial class frmMemoEditTest : DevExpress.XtraEditors.XtraForm
+    {
+        public frmMemoEditTest()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMemoEditTest_Load(object sender, EventArgs e)
+        {
+            this.memoEdit1.Text = 
+@"AAA
+BBB
+CCC
+DDD
+EEE
+FFF
+GGG
+HHH
+III
+JJJ
+KKK
+LLL
+MMM
+NNN
+OOO
+PPP
+";
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var searchText = txtSearch.Text;
+            var isContain = memoEdit1.Text.Contains(searchText);
+
+            if (!isContain)
+            {
+                XtraMessageBox.Show($"沒有找到{searchText}。");
+                return;
+            }
+
+            memoEdit1.MaskBox.MaskBoxSelectionStart = memoEdit1.Text.IndexOf(searchText);
+            memoEdit1.MaskBox.MaskBoxSelectionLength = searchText.Length;
+            memoEdit1.ScrollToCaret();
+        }
+    }
+}
